fbd3635ab9b92640c1b1537dd4cc3c1919175320
jmrjs-maker <54016123+jmrjs-maker@users.noreply.github.com>
2019-08-12 01:32:07 -0400

    Added website

---
+++ precache-manifest.649684db14782d08d1a3a7250c967060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70efea128415e8a7fbfa692a0808348b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c25989136da2e9fc1e",
+    "url": "/static/css/main.ba503aba.chunk.css"
+  },
+  {
+    "revision": "a21ce81ad0b85352646c",
+    "url": "/static/js/2.75978794.chunk.js"
+  },
+  {
+    "revision": "87c25989136da2e9fc1e",
+    "url": "/static/js/main.5353d6b1.chunk.js"
+  },
+  {
+    "revision": "534f6e5142d8ebbd3ee1",
+    "url": "/static/js/runtime~main.08c5833a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
